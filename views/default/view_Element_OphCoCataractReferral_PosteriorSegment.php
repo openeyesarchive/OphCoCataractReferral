1d67ad36b17960f58ae01ff6bc99548b2b0f3f6c
Mark <m@rkw.me>
2013-05-01 09:29:31 +0100

    fixed layout of posterior segment element

--- views/default/view_Element_OphCoCataractReferral_PosteriorSegment.php
+++ views/default/view_Element_OphCoCataractReferral_PosteriorSegment.php
@@ -1,15 +1,13 @@
 
 <h4 class="elementTypeName"><?php  echo $element->elementType->name ?></h4>
 
-<table class="subtleWhite normalText">
-	<tbody>
-		<tr>
-			<td width="30%"><?php echo CHtml::encode($element->getAttributeLabel('right_eye'))?>:</td>
-			<td><span class="big"><?php echo $element->right_eye?></span></td>
-		</tr>
-		<tr>
-			<td width="30%"><?php echo CHtml::encode($element->getAttributeLabel('left_eye'))?>:</td>
-			<td><span class="big"><?php echo $element->left_eye?></span></td>
-		</tr>
-	</tbody>
-</table>
+<div class="cols2 clearfix" style="margin-bottom: 2em;">
+	<div class="left">
+		<div style="margin-left: 10px;">
+			<?php echo $element->right_eye?>
+		</div>
+	</div>
+	<div class="right">
+		<?php echo $element->left_eye?>
+	</div>
+</div>

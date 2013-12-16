93fd8e7612558529faed18d58dd8e739f19ee982
Mark <m@rkw.me>
2013-12-16 13:06:41 +0000

    [OE-3856] removed unnecessary statements

--- migrations/m131205_134155_table_versioning.php
+++ migrations/m131205_134155_table_versioning.php
@@ -893,54 +893,30 @@ CREATE TABLE `ophcocataractreferral_visualacuity_unit_value_version` (
 	public function down()
 	{
 		$this->dropColumn('ophcocataractreferral_visualacuity_check_method','deleted');
-		$this->dropColumn('ophcocataractreferral_visualacuity_check_method_version','deleted');
 		$this->dropColumn('ophcocataractreferral_visualacuity_method','deleted');
-		$this->dropColumn('ophcocataractreferral_visualacuity_method_version','deleted');
 		$this->dropColumn('ophcocataractreferral_visualacuity_reading','deleted');
-		$this->dropColumn('ophcocataractreferral_visualacuity_reading_version','deleted');
 		$this->dropColumn('ophcocataractreferral_visualacuity_unit','deleted');
-		$this->dropColumn('ophcocataractreferral_visualacuity_unit_version','deleted');
 		$this->dropColumn('ophcocataractreferral_visualacuity_unit_value','deleted');
-		$this->dropColumn('ophcocataractreferral_visualacuity_unit_value_version','deleted');
 
 		$this->dropColumn('et_ophcocataractreferral_confirmation','deleted');
-		$this->dropColumn('et_ophcocataractreferral_confirmation_version','deleted');
 		$this->dropColumn('et_ophcocataractreferral_currentrefraction','deleted');
-		$this->dropColumn('et_ophcocataractreferral_currentrefraction_version','deleted');
 		$this->dropColumn('et_ophcocataractreferral_hpc','deleted');
-		$this->dropColumn('et_ophcocataractreferral_hpc_version','deleted');
 		$this->dropColumn('et_ophcocataractreferral_hpc_first_second_eye','deleted');
-		$this->dropColumn('et_ophcocataractreferral_hpc_first_second_eye_version','deleted');
 		$this->dropColumn('et_ophcocataractreferral_hpc_history','deleted');
-		$this->dropColumn('et_ophcocataractreferral_hpc_history_version','deleted');
 		$this->dropColumn('et_ophcocataractreferral_hpc_impact','deleted');
-		$this->dropColumn('et_ophcocataractreferral_hpc_impact_version','deleted');
 		$this->dropColumn('et_ophcocataractreferral_hpc_onset','deleted');
-		$this->dropColumn('et_ophcocataractreferral_hpc_onset_version','deleted');
 		$this->dropColumn('et_ophcocataractreferral_hpc_refraction','deleted');
-		$this->dropColumn('et_ophcocataractreferral_hpc_refraction_version','deleted');
 		$this->dropColumn('et_ophcocataractreferral_intraocularpressure','deleted');
-		$this->dropColumn('et_ophcocataractreferral_intraocularpressure_version','deleted');
 		$this->dropColumn('et_ophcocataractreferral_intraocularpressure_instrument','deleted');
-		$this->dropColumn('et_ophcocataractreferral_intraocularpressure_instrument_version','deleted');
 		$this->dropColumn('et_ophcocataractreferral_patientdetails','deleted');
-		$this->dropColumn('et_ophcocataractreferral_patientdetails_version','deleted');
 		$this->dropColumn('et_ophcocataractreferral_patientdetails_driving_status','deleted');
-		$this->dropColumn('et_ophcocataractreferral_patientdetails_driving_status_version','deleted');
 		$this->dropColumn('et_ophcocataractreferral_posteriorsegment','deleted');
-		$this->dropColumn('et_ophcocataractreferral_posteriorsegment_version','deleted');
 		$this->dropColumn('et_ophcocataractreferral_posteriorsegment_text','deleted');
-		$this->dropColumn('et_ophcocataractreferral_posteriorsegment_text_version','deleted');
 		$this->dropColumn('et_ophcocataractreferral_previous_ophthalmic_history','deleted');
-		$this->dropColumn('et_ophcocataractreferral_previous_ophthalmic_history_version','deleted');
 		$this->dropColumn('et_ophcocataractreferral_previousrefraction','deleted');
-		$this->dropColumn('et_ophcocataractreferral_previousrefraction_version','deleted');
 		$this->dropColumn('et_ophcocataractreferral_refraction_type','deleted');
-		$this->dropColumn('et_ophcocataractreferral_refraction_type_version','deleted');
 		$this->dropColumn('et_ophcocataractreferral_surgeryrefraction','deleted');
-		$this->dropColumn('et_ophcocataractreferral_surgeryrefraction_version','deleted');
 		$this->dropColumn('et_ophcocataractreferral_visualacuity','deleted');
-		$this->dropColumn('et_ophcocataractreferral_visualacuity_version','deleted');
 
 		$this->dropTable('et_ophcocataractreferral_confirmation_version');
 		$this->dropTable('et_ophcocataractreferral_currentrefraction_version');

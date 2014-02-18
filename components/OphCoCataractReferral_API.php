c36129f95b79fdc0ab52af68df5f9f6c094acde3
Mark <m@rkw.me>
2014-02-18 10:35:29 +0000

    refactored API class

--- components/OphCoCataractReferral_API.php
+++ components/OphCoCataractReferral_API.php
@@ -19,46 +19,44 @@
 
 class OphCoCataractReferral_API extends BaseAPI
 {
-	function getEventID($episode_id)
+	public function getRefractionForLatestCataractReferralInEpisode($episode_id)
 	{
-		$episode = Episode::model()->findByPk($episode_id);
 		if ($cataract_referral = EventType::model()->find('class_name=?',array('OphCoCataractReferral'))) {
-			$criteria = new CDbCriteria;
-			$criteria->compare('episode_id',$episode->id);
-			$criteria->compare('event_type_id',$cataract_referral->id);
-			$criteria->limit = 1;
-			$criteria->order = 'created_date desc';
-		}
-		$event= Event::model()->find($criteria);
-		return $event->id;
-	}
-
-	public function getRefractionElement($episode_id)
-	{
-		return (array)Element_OphCoCataractReferral_CurrentRefraction::model()->find('event_id=?',array($this->getEventID($episode_id)))->attributes;
-	}
-
-	public function getHistory($episode_id)
-	{
-		if ($hpc = Element_OphCoCataractReferral_Hpc::model()->find('event_id=?',array($this->getEventID($episode_id)))) {
-			$history = array();
-			$hpc->history && $history[] = $hpc->history;
-			$hpc->impact && $history[] = $hpc->impact;
-			$history[] = $hpc->eye->name.' eye'.($hpc->eye_id == 3 ? 's' : '');
-			$history[] = $hpc->onset->name;
-			$history[] = $hpc->first_second_eye->name;
-			return implode(', ',$history);
+			if ($event = $this->getMostRecentEventInEpisode($episode_id, $cataract_referral->id)) {
+				return (array)Element_OphCoCataractReferral_CurrentRefraction::model()->find('event_id=?',array($event->id))->attributes;
+			}
 		}
 	}
 
-	public function getVisualAcuityElement($episode_id)
+	public function getHistoryForLatestCataractReferralInEpisode($episode_id)
 	{
-		$element = Element_OphCoCataractReferral_VisualAcuity::model()->find('event_id=?',array($this->getEventID($episode_id)));
-		$readings = (array)$element->attributes;
-		$readings['left_readings']=$element->getFormReadings('left');
-		$readings['right_readings']=$element->getFormReadings('right');
+		if ($cataract_referral = EventType::model()->find('class_name=?',array('OphCoCataractReferral'))) {
+			if ($event = $this->getMostRecentEventInEpisode($episode_id, $cataract_referral->id)) {
+				if ($hpc = Element_OphCoCataractReferral_Hpc::model()->find('event_id=?',array($event->id))) {
+					$history = array();
+					$hpc->history && $history[] = $hpc->history;
+					$hpc->impact && $history[] = $hpc->impact;
+					$history[] = $hpc->eye->name.' eye'.($hpc->eye_id == 3 ? 's' : '');
+					$history[] = $hpc->onset->name;
+					$history[] = $hpc->first_second_eye->name;
+					return implode(', ',$history);
+				}
+			}
+		}
+	}
 
-		return $readings;
+	public function getVisualAcuityReadingsForLatestCataractReferralInEpisode($episode_id)
+	{
+		if ($cataract_referral = EventType::model()->find('class_name=?',array('OphCoCataractReferral'))) {
+			if ($event = $this->getMostRecentEventInEpisode($episode_id, $cataract_referral->id)) {
+				if ($element = Element_OphCoCataractReferral_VisualAcuity::model()->find('event_id=?',array($event->id))) {
+					$readings = (array)$element->attributes;
+					$readings['left_readings']=$element->getFormReadings('left');
+					$readings['right_readings']=$element->getFormReadings('right');
 
+					return $readings;
+				}
+			}
+		}
 	}
 }
